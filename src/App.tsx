24ad6e43015e87fa61fd424ad0a665b162981c0f
Subham Mondal <subhamoffi@gmail.com>
2025-08-21 12:18:36 +0530

    Made the footer of with copyright stuff and all

--- src/App.tsx
+++ src/App.tsx
@@ -34,9 +34,25 @@ function App() {
             </Routes>
           </Container>
         </div>
-        <footer className="w-full h-40 mt-4 bg-black">
-          <div className="text-center max-w-3xl text-white mx-auto h-full">
-            Hello world
+        <footer className="w-full bg-black text-center  text-white py-6 mt-8">
+          <div className="max-w-3xl mx-auto text-sm px-4 flex flex-col items-center justify-between gap-4">
+            {/* Copyright */}
+            <p className="text-gray-400 font-toreadore">
+              © {new Date().getFullYear()} Bond Store. All rights reserved.
+            </p>
+
+            {/* Developer credit */}
+            <p className="text-gray-400">
+              Developed & Maintained by{" "}
+              <a
+                href="https://twitter.com/codersubham"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white hover:underline font-medium"
+              >
+                CoderSubham
+              </a>
+            </p>
           </div>
         </footer>
       </Router>
